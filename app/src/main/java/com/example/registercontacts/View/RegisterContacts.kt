fd04664f8cc53448723d22b06c7e9e529713cc19
Bruno Oliveira <bruno.osilva@neon.com.br>
2023-04-11 20:19:27 -0300

    :sparkles: Included dynamic register contact on firestore

--- app/src/main/java/com/example/registercontacts/View/RegisterContacts.kt
+++ app/src/main/java/com/example/registercontacts/View/RegisterContacts.kt
@@ -2,12 +2,13 @@ package com.example.registercontacts.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.registercontacts.R
 import com.example.registercontacts.databinding.ActivityRegisterContactsBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.UUID
 
 class RegisterContacts : AppCompatActivity() {
     private var db = Firebase.firestore
@@ -19,19 +20,42 @@ class RegisterContacts : AppCompatActivity() {
         binding = ActivityRegisterContactsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        getFields()
+    }
 
+    private fun getFields() {
         binding.btnSalvar.setOnClickListener(View.OnClickListener {
-            Log.e("asd", "asd")
-            val city = hashMapOf(
-                "name" to "Los Angeles",
-                "state" to "CA",
-                "country" to "USA"
+
+            val email = binding.emailInput.text.toString()
+            val name = binding.nameInput.text.toString()
+            val phone = binding.phoneInput.text.toString()
+
+            val contact = hashMapOf(
+                "name" to name,
+                "phone" to phone,
+                "email" to email
             )
 
-            db.collection("contacts").document("register_contacts")
-                .set(city)
-                .addOnSuccessListener { Log.d("---->", "DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w("---->", "Error writing document", e) }
+            saveContactsDB(contact)
         })
+    }
+
+    private fun saveContactsDB(contact: HashMap<String, String>) {
+        val id = UUID.randomUUID().toString()
+
+        db.collection("contacts").document(id)
+            .set(contact)
+            .addOnSuccessListener {
+                Toast.makeText(
+                    this.applicationContext,
+                    "Contato salvo com sucesso!!",
+                    Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    this.applicationContext,
+                    "Falha ao salvar contato, tente novamente!",
+                    Toast.LENGTH_LONG).show()
+            }
     }
 }
